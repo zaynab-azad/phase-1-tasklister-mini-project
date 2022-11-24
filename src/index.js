e05ed9e695e6a8cf8971a1ef93a215156e1e8e5b
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-24 11:42:40 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createNewList = document.querySelector("input[type='submit']");
+  const input = document.querySelector("#new-task-description");
+  const list = document.querySelector("#tasks");
+  createNewList.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input.value != "") {
+      const button = document.createElement("button");
+      button.textContent = "X";
+      const newTask = document.createElement("li");
+      newTask.textContent = input.value;
+      list.appendChild(newTask);
+      newTask.appendChild(button);
+      input.value = "";
+      button.addEventListener("click", () => newTask.remove());
+    } else {
+      alert("Enter a value");
+    }
+  });
 });
